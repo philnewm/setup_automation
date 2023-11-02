a34f0d6adb81dc3dbb9f68defdc426f9645afdb5
philnewm <Philippkd8@gmail.com>
2023-11-02 16:01:43 +0100

    cleaned up grub role

---
+++ something.py
@@ -0,0 +1,15 @@
+def things(one: int, two : int) -> None:
+    """_summary_
+
+    _extended_summary_
+
+    Examples:
+        >>> foo("Hello World")
+        >>> pls keep examples short
+
+    Args:
+        one (int): _description_
+        two (int): _description_
+    """
+    print("soemthing")
+    
