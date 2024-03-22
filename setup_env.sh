7409b8422836cbfcaf77cf8c5fc49513bb044875
philnewm <Philippkd8@gmail.com>
2024-03-23 00:42:36 +0100

    troubleshooting vagrant

--- setup_env.sh
+++ setup_env.sh
@@ -17,8 +17,10 @@ export TMPDIR=/mnt/c/tmp
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install vagrant -y
+sudo vagrant plugin install vagrant-libvirt
 mkdir ~/Documents
 nano ~/Documents/vault.pw
+vagrant plugin install virtualbox_WSL2
 
 # required tools
 python3 -m pip install molecule ansible-core
