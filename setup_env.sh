3209448e326e3f7749c9324155d035c131363d24
PhilNewm <Philippkd8@gmail.com>
2024-03-23 23:16:21 +0100

    removed bsolete data from config
    added few more commands to setup

--- setup_env.sh
+++ setup_env.sh
@@ -4,37 +4,39 @@ sudo apt update
 sudo apt install python3.10-venv -y
 python3 -m venv ~/.venv/ansible_env/
 source ~/.venv/ansible_env/bin/activate
-sudo apt install virtualbox -y
+
+# worksapce settings:
+{
+    "ansible.python.interpreterPath": "~/.venv/ansible_env/bin/python",
+    "python.defaultInterpreterPath": "~/.venv/ansible_env/bin/python"
+}
 
 # pre-rquirements ubuntu
 sudo apt install python3-pip libssl-dev -y
-sudo apt install docker.io
-sudo chmod 666 /var/run/docker.sock
-export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
-export TMPDIR=/mnt/c/tmp
+# append those two lines into ~/.bashrc
+echo 'export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"' >> ~/.bashrc
+echo 'export PATH="$PATH:/mnt/c/Program Files/Oracle/VirtualBox"' >> ~/.bashrc
+
+# now reload the ~/.bashrc file
+source ~/.bashrc
 
 # vagrant
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install vagrant -y
-sudo vagrant plugin install vagrant-libvirt
+vagrant plugin install virtualbox_WSL2
+
 mkdir ~/Documents
 nano ~/Documents/vault.pw
-vagrant plugin install virtualbox_WSL2
+
 
 # required tools
 python3 -m pip install molecule ansible-core
-python3 -m pip install --upgrade --user setuptools
+python3 -m pip install --upgrade setuptools
 python3 -m pip install molecule ansible-lint
 python3 -m pip install "molecule-plugins[vagrant]"
 
 # ansible packages
 ansible-galaxy collection install ansible.posix
 
-# libvirt
-sudo apt install qemu-kvm libvirt-daemon-system -y
-pip install molecule-libvirt
-
-# docker
-pip install docker
-
+sudo apt install sshpass
